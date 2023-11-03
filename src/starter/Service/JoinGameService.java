05c79872fd905ab3dc52121b0b690a6b8da94ad0
anyssakayla <anyssakayla@gmail.com>
2023-11-02 22:17:12 -0600

    14 tests pass, 9 fail

--- src/starter/Service/JoinGameService.java
+++ src/starter/Service/JoinGameService.java
@@ -20,8 +20,8 @@ public class JoinGameService {
    * @param request The join game request to the database
    * @return JoinGameResult object with information on if the request was successful
    * */
-  public JoinGameResult joinGame(JoinGameReq request, String auth) throws IOException, DataAccessException {
-    AuthDao authDao = new AuthDao();
+  public JoinGameResult joinGame(JoinGameReq request, String auth) throws DataAccessException {
+    AuthDao authDao = new AuthDao(); //TODO: Fix gameID like username with Dao's
     GameDao gameDao = new GameDao();
     ChessGame.TeamColor teamColor;
     String requestAuth = request.getAuthToken();
@@ -38,51 +38,50 @@ public class JoinGameService {
       return result;
     }
 
-    if(request.getGameID() < 999){
-      result.setMessage("Error: bad request");
-      return result;
-    }
-
-    if(requestColor == null){
-      gameDao.claimSpot(request.getUsername(), request.getGameID(), null);
-      result.setTeamColor(null);
-      result.setUsername(request.getUsername());
-      result.setGameID(request.getGameID());
-    }
-
+//    if(request.getGameID() < 999){
+//      result.setMessage("Error: bad request");
+//      return result;
+//    }
     if(request.getTeamColor() != null){ //if it is null, it will be an error to do lowercase
       notNullColor = request.getTeamColor().toLowerCase(Locale.ROOT);
     }
 
-     if(notNullColor.equals("black")){ //if black, set teamcolor to black
+    if(requestColor == null){
+      gameDao.claimSpot(authDao.getAuth(auth).getUsername(), request.getGameID(), null);
+      result.setTeamColor(null);
+      result.setUsername(authDao.getAuth(auth).getUsername());
+      result.setGameID(request.getGameID());
+    }
+
+     else if(notNullColor.equals("black")){ //if black, set teamcolor to black
        teamColor = ChessGame.TeamColor.BLACK;
        if(joinGame.getBlackUsername() == null){ //make sure black player doesn't already exist
          result.setTeamColor("black");
-         result.setUsername(request.getUsername());
+         result.setUsername(authDao.getAuth(auth).getUsername());
          result.setGameID(request.getGameID());
-         gameDao.claimSpot(request.getUsername(), request.getGameID(), teamColor);
+         gameDao.claimSpot(authDao.getAuth(auth).getUsername(), request.getGameID(), teamColor);
        }
        else{
-         result.setMessage("This spot is already taken");
-         throw new IOException("Error: already taken");
+         result.setMessage("Error: already taken");
+         //throw new IOException("Error: already taken");
        }
      }
      else if(notNullColor.equals("white")){
        teamColor = ChessGame.TeamColor.WHITE;
        if(joinGame.getWhiteUsername() == null){
          result.setTeamColor("white");
-         result.setUsername(request.getUsername());
+         result.setUsername(authDao.getAuth(auth).getUsername());
          result.setGameID(request.getGameID());
-         gameDao.claimSpot(request.getUsername(), request.getGameID(), teamColor);
+         gameDao.claimSpot(authDao.getAuth(auth).getUsername(), request.getGameID(), teamColor);
        }
        else{
-         result.setMessage("This spot is already taken");
-         throw new IOException("Error: this spot is already taken");
+         result.setMessage("Error: already taken");
+         //throw new IOException("Error: this spot is already taken");
        }
      }
      else{
        result.setMessage("Error: bad request");
-       throw new IOException("Error: bad request");
+      // throw new IOException("Error: bad request");
      }
 
      result.setGameID(request.getGameID());
